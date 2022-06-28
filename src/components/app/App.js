04c2cf412fe381aada150adbeb562556fb882ad7
AlexFeeD <alexdevelopfrontent@gmail.com>
2022-06-28 19:59:54 +0300

    add new animation, filters and edit todos functionality

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,6 +10,8 @@ import './App.css';
 function App() {
     const [data, setData] = useLocalStorage('data', [])
 
+    const [todosFilter, setTodosFilter] = useState('all')
+
     const [inputText, setInputText] = useState('');
 
     const getWeekDay = (date) => {
@@ -46,6 +48,18 @@ function App() {
         setData(newTodoList);
     }
 
+    const changeTodoItemText = (id, editedText) => {
+        const newTodoList = data.map(todoItem => {
+            if (todoItem.id === id) {
+                return {...todoItem, text: editedText}
+            } else {
+                return todoItem
+            }
+        })
+
+        setData(newTodoList);
+    }
+
     const onEnterDown = (e) => {
         if (e.key === 'Enter') {
             addTodoItem()
@@ -56,11 +70,29 @@ function App() {
         if (list && list.length > 0) {
             return list.map(itemData => {
                 return <TodoItem key={itemData.id} id={itemData.id} text={itemData.text}
-                                 handleChange={onCheckboxChanged} handleDelete={deleteTodoItem}
-                                 isCompleted={itemData.isCompleted}/>
+                                 onCheckboxChanged={onCheckboxChanged} handleDelete={deleteTodoItem}
+                                 isCompleted={itemData.isCompleted}
+                                 changeTodoItemGlobalText={changeTodoItemText}/>
             })
         }
     }
+
+    const renderFilteredTodos = (filter, list = data) => {
+        if (list && list.length > 0) {
+            switch (filter) {
+                case 'all':
+                    return data
+                case 'completed':
+                    return list.filter(todo => todo.isCompleted === true)
+                case 'uncompleted':
+                    return list.filter(todo => todo.isCompleted === false)
+                default:
+                    return
+            }
+        }
+    }
+
+    const filteredTodos = renderFilteredTodos(todosFilter);
 
     return (
         <div className="App__wrap">
@@ -77,9 +109,25 @@ function App() {
                         <button className="todo__btn" onClick={addTodoItem}>+</button>
                     </div>
                     <ul className="todo__list">
-                        {renderTodoItems(data)}
+                        {renderTodoItems(filteredTodos)}
                     </ul>
                 </div>
+                <footer className="footer">
+                    <div className="footer__filters">
+                        <button onClick={() => setTodosFilter('all')}
+                                className={`filter__item ${todosFilter === 'all' ? 'activeFilter' : ''}`}>
+                            All
+                        </button>
+                        <button onClick={() => setTodosFilter('uncompleted')}
+                                className={`filter__item ${todosFilter === 'uncompleted' ? 'activeFilter' : ''}`}>
+                            Uncompleted
+                        </button>
+                        <button onClick={() => setTodosFilter('completed')}
+                                className={`filter__item ${todosFilter === 'completed' ? 'activeFilter' : ''}`}>
+                            Completed
+                        </button>
+                    </div>
+                </footer>
             </div>
         </div>
     );
--- src/components/todoItem/TodoItem.js
+++ src/components/todoItem/TodoItem.js
@@ -1,12 +1,57 @@
+import {useEffect, useRef, useState} from "react";
+
 import './todoItem.css';
 
-const TodoItem = ({text, isCompleted, id, handleChange, handleDelete}) => {
-    return(
-        <li className="todoItem">
-            <input checked={isCompleted} onChange={() => handleChange(id)}
+
+const TodoItem = ({text, isCompleted, id, onCheckboxChanged, handleDelete, changeTodoItemGlobalText}) => {
+    const [editMode, setEditMode] = useState(false);
+    const [inputLocalText, setInputLocalText] = useState(text);
+
+    const editInputRef = useRef();
+
+    useEffect(() => {
+        if (editInputRef.current && editMode === true) {
+            editInputRef.current.setSelectionRange(inputLocalText.length, inputLocalText.length);
+            editInputRef.current.focus();
+        }
+    }, [editMode]);
+
+    const onEditModeSwitchedOff = () => {
+        setEditMode(false);
+
+        changeTodoItemGlobalText(id, inputLocalText);
+    }
+
+    const onEnterDown = (e) => {
+        if (e.key === 'Enter') {
+            onEditModeSwitchedOff()
+        }
+    }
+
+    const editInput = (
+        <>
+            <input type="text" value={inputLocalText}
+                   onChange={(e) => setInputLocalText(e.target.value)}
+                   onKeyDown={onEnterDown} onBlur={onEditModeSwitchedOff}
+                   ref={editInputRef}
+                   className="edit__input"/>
+        </>
+    )
+
+    const todoContent = (
+        <>
+            <input checked={isCompleted} onChange={() => onCheckboxChanged(id)}
                    type="checkbox" className="todoItem__checkbox"/>
-            <label className="todoItem__text">{text}</label>
+            <label onDoubleClick={() => setEditMode(true)}
+                   className={`todoItem__text ${isCompleted ? 'labelAnimation' : ''}`}>{text}</label>
             <button onClick={() => handleDelete(id)} className="todoItem__delete"/>
+        </>
+    )
+
+    const content = editMode ? editInput : todoContent;
+    return (
+        <li className="todoItem">
+            {content}
         </li>
     )
 }
